3b046989c79eaf12af85625b7c91359e6d4711c5
Jefferson Lee <jefferson.lee@gmail.com>
2018-02-21 10:08:24 -0500

    added server

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	connect = require('gulp-connect');
 var livereload = require('gulp-livereload')
 var uglify = require('gulp-uglifyjs');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
 // var imagemin = require('gulp-imagemin');
 // var pngquant = require('imagemin-pngquant');
-
-
-
 
 // gulp.task('imagemin', function () {
 //     return gulp.src('images/*')
@@ -19,30 +18,51 @@ var sourcemaps = require('gulp-sourcemaps');
 //         }))
 //         .pipe(gulp.dest('./images'));
 // });
+gulp.task('default', function(){
+	console.log('hi');
+})
 
+ gulp.task('connect', function(){
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('*.html')
+    .pipe(gulp.dest('app'))
+    .pipe(livereload.reload());
+});
 
 gulp.task('sass', function () {
-  gulp.src('sass/**/*.scss')
+  gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write(''))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest('app/'))
+    .pipe(livereload());
 });
 
 
 gulp.task('uglify', function() {
-  gulp.src('lib/*.js')
+  gulp.src('./lib/*.js')
     .pipe(uglify('main.min.js'))
     .pipe(gulp.dest('js/'))
 });
 
 gulp.task('watch', function(){
+
+    var server = livereload();
     livereload.listen();
 
-    gulp.watch('sass/**/*.scss', ['sass']);
-    gulp.watch('lib/*.js', ['uglify']);
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./lib/*.js', ['uglify']);
+    gulp.watch(['/app/*.html'], ['html']);
     gulp.watch(['style.css', 'js/*.js'], function (files){
         livereload.changed(files)
     });
 });
+
+gulp.task('default', ['connect', 'watch', 'sass', 'uglify']);
