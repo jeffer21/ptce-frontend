6c64ab4da4c7b2423e347d9213bc9752f28821a3
Alan Joseph <ajoseph0501@hotmail.com>
2018-02-23 11:03:59 -0500

    Fixing Popup on Landing page

--- app/js/landing.js
+++ app/js/landing.js
@@ -67,25 +67,11 @@ function next() {
     var chosen =[];
 }
 
-//Modal before quiz
-// Get the modal
-var modal = document.getElementById('myModal');
+function close () {
+    document.getElementById("myModal").style.display = "none";
+}
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+function activityStart () {
+    document.getElementById("myModal").style.display = "none";
+}
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-
-//// When the user clicks on <span> (x), close the modal
-//span.onClick = function() {
-//    modal.style.display = "none";
-//};
-//
-//// When the user clicks anywhere outside of the modal, close it
-//window.onClick = function(event) {
-//    if (event.target == modal) {
-//        modal.style.display = "none";
-//    }
-//};
