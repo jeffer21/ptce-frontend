a92db03dd9bd348b5f26416d91856e4b734eae57
Alan Joseph <ajoseph0501@hotmail.com>
2018-02-26 17:15:24 -0500

    Working on previous button as well as setup based on type. Also storing questions and checking answers.
    Landing html, js
    Results html, js

---
+++ app/js/results.js
@@ -0,0 +1,9 @@
+/**
+ * Created by AlanJoseph on 2/26/2018.
+ */
+$( document ).ready(function() {
+    setTimeout(function(){
+        console.log('Bye Results Page!');
+        window.location.replace('index.html');
+    }, 3000);
+});
