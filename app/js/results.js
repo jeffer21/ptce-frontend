559b7ce6259b3fb81ffdd3a3dd08da2bed6fe2b7
Alan Joseph <ajoseph0501@hotmail.com>
2018-03-01 15:31:13 -0500

    Progress bar changes. New Final Exam Setup
    Landing JS
    Results JS

--- app/js/results.js
+++ app/js/results.js
@@ -1,9 +1,16 @@
 /**
  * Created by AlanJoseph on 2/26/2018.
  */
+var amount = 0;
 $( document ).ready(function() {
     setTimeout(function(){
         console.log('Bye Results Page!');
-        window.location.replace('index.html');
+
+        if(amount == 0) {
+            amount++;
+            window.location.replace('landing.html');
+        } else {
+            window.location.replace('index.html');
+        }
     }, 3000);
 });
